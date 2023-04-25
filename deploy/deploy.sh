8f527580fb1f2978cce5de33e3b12087a71444de
Patrick Catach <p.catach@iwoca.co.uk>
2023-04-25 15:13:58 -0300

    Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+mv /tmp/config.json /config.json
 sudo apt update
 sudo apt install -y python3-venv
 python3 -m venv /env
 /env/bin/pip install --upgrade pip
 /env/bin/pip install /tmp/magg-1.0.0.tar.gz
 
-# this is temporary
-echo {} > /config.json
-# run every day at 14:21
-echo "21 14 * * * /usr/bin/python -m magg --renew --mail" | crontab -
+# run every day at 18:30
+echo "30 18 * * * /env/bin/python -m magg --renew --mail --config=/config.json >> /var/log/magg.log 2>&1" | crontab -
+
