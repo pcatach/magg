663891459c9f17d9cd966aacda75795056530af1
Patrick Catach <p.catach@iwoca.co.uk>
2023-04-30 23:37:44 -0300

    Tidy up image setup script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -x
 
-# Install necessary dependencies
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y python3.10-venv
-
 # Install python package
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y python3.10-venv
 sudo mkdir /opt/magg
 sudo chown -R ubuntu /opt/magg
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
@@ -17,17 +15,18 @@ python3 -m venv /opt/magg/env
 
 # Setup sudo to allow no-password sudo for "magg" group and adding "magg" user
 sudo groupadd -r magg
-sudo useradd -s /bin/bash -g magg magg
+sudo useradd -s /bin/bash -d /home/magg -m -g magg magg
 sudo cp /etc/sudoers /etc/sudoers.orig
 echo "magg  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/magg
+sudo chown -R magg /opt/magg
 
 # Installing SSH key
+sudo -i -u magg bash << EOF
 mkdir -p /home/magg/.ssh
 chmod 700 /home/magg/.ssh  
 cp /tmp/aws-magg.pub /home/magg/.ssh/authorized_keys
 chmod 600 /home/magg/.ssh/authorized_keys
-chown -R magg /home/magg/.ssh
-chown -R magg /opt/magg
+EOF
 
 # run every Wednesday at 15:30
 echo "30 15 * * 3 /opt/magg/env/bin/python -m magg --renew --mail --mail-from=$MAIL_FROM --mail-to=$MAIL_TO >> /var/log/magg.log 2>&1" | crontab -
